107930df43cc306213700c28fda58e0c4aaf76a6
Kimberly Aguilar <757379@student.dusd.net>
2023-11-09 15:26:37 -0800

    Added collider and health script
    Added new forms of code to ruby

--- RubysAdventureAguilar/Assets/Scripts/RubyController.cs
+++ RubysAdventureAguilar/Assets/Scripts/RubyController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public int maxHealth = 5;
+    int currentHealth;
+    
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -12,16 +15,28 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
-        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 5.0f * vertical * Time.deltaTime;
-        transform.position = position;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 4.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 4.0f * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
